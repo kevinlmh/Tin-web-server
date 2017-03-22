eb3ab0f5acde89030a2f7c2bc490cb916f6f1898
Kevin Liu <minghui.liu@trincoll.edu>
2017-03-21 21:46:51 -0400

    Finish rest of RIO functions

--- rio.c
+++ rio.c
@@ -5,6 +5,8 @@
  * 
  **********************************************************/
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include "rio.h"
@@ -117,6 +119,98 @@ ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n) {
 /*
  * rio_readlineb - robustly read a text line (buffered)
  */
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen) {
+	int n, rc;
+	char c, *bufp = usrbuf;
+	
+	for (n = 1; n < maxlen; n++) {
+		if ((rc = rio_read(rp, &c, 1)) == 1) {
+			*bufp++ = c;
+			if (c == '\n') {
+				n++;
+				break;
+			}
+		} else if (rc == 0) {
+			if (n == 1)	/* EOF, no data read */
+				return 0;
+			else		/* EOF, some data read */
+				break;
+		} else			/* Error */
+			return -1;
+	}
+	*bufp = 0;
+	return n-1;
+}
+
+
+/******************************************************
+ * Wrappers for Robust I/O routines
+ *******************************************************/
+ssize_t Rio_readn(int fd, void *ptr, size_t nbytes) {
+	ssize_t n;
+	if ((n = rio_readn(fd, ptr, nbytes)) < 0) {
+		fprintf(stderr, "Rio_readn error: %s\n", strerror(errno));
+		exit(0);
+	}
+}
+
+void Rio_writen(int fd, void *usrbuf, size_t n) {
+	if (rio_writen(fd, usrbuf, n) != n) {
+		fprintf(stderr, "Rio_writen error: %s\n", strerror(errno));
+		exit(0);
+	}
+}
+
+void Rio_readinitb(rio_t *rp, int fd) {
+	rio_readinitb(rp, fd);
+}
+
+ssize_t Rio_readnb(rio_t *rp, void *usrbuf, size_t n) {
+	ssize_t rc;
+
+	if ((rc = rio_readnb(rp, usrbuf, n)) < 0) {
+		fprintf(stderr, "Rio_readnb error: %s\n", strerror(errno));
+		exit(0);
+	}
+	return rc;
+}
+
+ssize_t Rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen) {
+	ssize_t rc;
+
+	if ((rc = rio_readlineb(rp, usrbuf, maxlen)) < 0) {
+		fprintf(stderr, "Rio_readlineb error: %s\n", strerror(errno));
+		exit(0);
+	}
+	return rc;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
